9f97132bcc66a4440f6942b7242a5d2b6e19d5e7
Tim Murray <timmurray@google.com>
2016-07-12 13:30:16 -0700

    mido: Enable FIFO UI scheduling by default
    bug 24503801
    
    Change-Id: I3ae54e8af22fa3f26a58d39c24c2ee70f0732646

--- prop.mk
+++ prop.mk
@@ -132,7 +132,8 @@ vendor.vidc.enc.disable_bframes=1
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so \
-ro.vendor.qti.sys.fw.bg_apps_limit=60
+ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+sys.use_fifo_ui=1
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
