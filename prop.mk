104818f805f595cf0a1575c22ce0c3d44450da01
Daniel Sikorski <dadi.sikorski@gmail.com>
2017-08-15 00:00:44 +0200

    mido: dex2oat: use 8 cores instead of 4
    use more cores for faster app installation
    
    Change-Id: I7045673a28eed043ec45013b1abc48e5f3a5f7ba
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- prop.mk
+++ prop.mk
@@ -123,7 +123,7 @@ vendor.vidc.enc.disable_bframes=1
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.fw.dex2oat_thread_count=4 \
+ro.sys.fw.dex2oat_thread_count=8 \
 ro.vendor.extension_library=libqti-perfd-client.so \
 ro.vendor.qti.sys.fw.bg_apps_limit=60
 
