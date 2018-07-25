aleha-druga/android_device_xiaomi_mido-old

mido: Add OEM privileged app permissions
Signed-off-by: Arvind Mukund <armu30@gmail.com>

device.mk
@@ -290,6 +290,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Privileged app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson

MiracleDROID-HnT/android_device_xiaomi_mido

device.mk
@@ -290,6 +290,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Privileged app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Properties
 -include device/xiaomi/mido/prop.mk
 